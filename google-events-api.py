47d4b9a6a9732857f1002678b4661333ccefdd30
John Cole <29712567+johnisanerd@users.noreply.github.com>
2026-04-15 21:26:39 +0500

    Update google-events-api.py

--- google-events-api.py
+++ google-events-api.py
@@ -21,13 +21,14 @@ run_input = {
     "q": "events in South Bend Indiana",
     "location": "South Bend, Indiana",
     "advanced": "date:today",
-    "max_pages": 3,
+    "max_pages": 1,
 }
 
-# Run the Actor and wait for it to finish
-run = client.actor("johnvc/google-events-api---access-google-events-data").call(run_input=run_input)
+for each in range(0, 100):
+    # Run the Actor and wait for it to finish
+    run = client.actor("johnvc/google-events-api---access-google-events-data").call(run_input=run_input)
 
-# Fetch and print Actor results from the run's dataset (if there are any)
-print("💾 Check your data here: https://console.apify.com/storage/datasets/" + run["defaultDatasetId"])
-for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-    print(item)
+    # Fetch and print Actor results from the run's dataset (if there are any)
+    print("💾 Check your data here: https://console.apify.com/storage/datasets/" + run["defaultDatasetId"])
+    for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+        print(item)
